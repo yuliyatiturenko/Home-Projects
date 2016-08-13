a0c2a7338006f149fe39d89a684dfd6ea871736e
Yuliya.Titurenko <yuliya.titurenko@gmal.com>
2016-08-13 12:23:22 +0300

    sintaxis fix

--- homework1/blijayshee_k_10/src/blijayshee_k_10.java
+++ homework1/blijayshee_k_10/src/blijayshee_k_10.java
@@ -1,15 +1,21 @@
 
 public class blijayshee_k_10 {
-    public static void main(String[] args)
-    {
-        int a=10;
-        int b=13;
-        int c=8;
-        int d=Math.abs(a-b);
-        int e =Math.abs(a-c);
-            if (d==e) System.out.println("b и c  одинаково близки к 10");
-                else if (d<e) System.out.println("b ближайшее к 10");
-                    else System.out.println("c ближайшее к 10");
-
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 13;
+        int c = 8;
+        int d = Math.abs(a - b);
+        int e = Math.abs(a - c);
+        if (d == e) {
+            System.out.println("b и c  одинаково близки к 10");
+        } else {
+            if (d < e) {
+                System.out.println("b ближайшее к 10");
+            } else {
+                System.out.println("c ближайшее к 10");
+            }
+        }
     }
+
 }
+
